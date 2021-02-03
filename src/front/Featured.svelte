dd4da59554377d96252b8f7625ce642cd90e0304
sebastianjof <sebastianjof@gmail.com>
2021-02-02 22:17:05 -0300

    Add files via upload

--- src/front/Featured.svelte
+++ src/front/Featured.svelte
@@ -29,7 +29,7 @@ import {OpenModalFicha} from "./Ficha.svelte"
 
     <div class="uk-position-relative uk-visible-toggle uk-light" tabindex="-1">
 
-        <ul class="uk-slider-items uk-child-width-1-4@s uk-grid">
+        <ul class="uk-child-width-1-2@s uk-child-width-1-3@m uk-slider-items uk-child-width-1-4@s uk-grid">
         {#each autos as item}    
             <li>
                 <div class="uk-card uk-card-default uk-card-hover">
@@ -70,8 +70,8 @@ import {OpenModalFicha} from "./Ficha.svelte"
         {/each}
         </ul>
 
-        <a class="uk-position-center-left uk-position-small uk-hidden-hover" href="#" uk-slidenav-previous uk-slider-item="previous"></a>
-        <a class="uk-position-center-right uk-position-small uk-hidden-hover" href="#" uk-slidenav-next uk-slider-item="next"></a>
+        <a class="uk-dark uk-position-top-left" href="#" uk-slidenav-previous uk-slider-item="previous"></a>
+        <a class="uk-dark uk-position-top-right" href="#" uk-slidenav-next uk-slider-item="next"></a>
 
     </div>
 
--- src/front/Todos.svelte
+++ src/front/Todos.svelte
@@ -30,7 +30,7 @@ import {OpenModalFicha} from "./Ficha.svelte"
 
     <div class="uk-position-relative uk-visible-toggle uk-light" tabindex="-1">
 
-        <ul class="uk-slider-items uk-child-width-1-4@s uk-grid">
+        <ul class="uk-child-width-1-2@s uk-child-width-1-3@m uk-slider-items uk-child-width-1-4@s uk-grid">
         {#each autos as item}    
             <li>
                 <div class="uk-card uk-card-default uk-card-hover">
@@ -73,8 +73,8 @@ import {OpenModalFicha} from "./Ficha.svelte"
         {/each}
         </ul>
 
-        <a class="uk-position-center-left uk-position-small uk-hidden-hover" href="#" uk-slidenav-previous uk-slider-item="previous"></a>
-        <a class="uk-position-center-right uk-position-small uk-hidden-hover" href="#" uk-slidenav-next uk-slider-item="next"></a>
+        <a class="uk-position-top-left uk-position-small uk-hidden-hover" href="#" uk-slidenav-previous uk-slider-item="previous"></a>
+        <a class="uk-position-top-right uk-position-small uk-hidden-hover" href="#" uk-slidenav-next uk-slider-item="next"></a>
 
     </div>
 
